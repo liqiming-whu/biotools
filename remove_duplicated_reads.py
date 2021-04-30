0062f53a3e83e642da3bf7ee81f4755efc7b6984
liqiming-whu <liqiming@whu.edu.cn>
2021-04-30 18:33:46 +0800

    update

--- remove_duplicated_reads.py
+++ remove_duplicated_reads.py
@@ -3,17 +3,20 @@
 Remove duplicated reads for paired-end sequencing. python >= 3.8
 Author: liqiming@whu.edu.cn
 """
+import os
 import gzip
 import argparse
-from collections import defaultdict
 
 
-def main(read1, read2, out_read1, out_read2):
-    unique = defaultdict(int)
+def main(read1, read2, out_read1, out_read2, dup_read1, dup_read2):
+    unique = set()
     input_reads = 0
     output_reads = 0
     while id1 := read1.readline():
         id2 = read2.readline()
+        name1 = id1.decode().split()[0]
+        name2 = id2.decode().split()[0]
+        assert name1 == name2
         seq1 = read1.readline()
         seq2 = read2.readline()
         symbol1 = read1.readline()
@@ -23,19 +26,22 @@ def main(read1, read2, out_read1, out_read2):
         input_reads += 1
 
         whole_reads = seq1 + seq2
-        if unique[whole_reads]:
-            continue
+        if whole_reads in unique:
+            dup_read1.write(id1+seq1+symbol1+qual1)
+            dup_read2.write(id2+seq2+symbol2+qual2)
         else:
             out_read1.write(id1+seq1+symbol1+qual1)
             out_read2.write(id2+seq2+symbol2+qual2)
-            unique[whole_reads] = 1
+            unique.add(whole_reads)
             output_reads += 1
     out_read1.close()
     out_read2.close()
+    dup_read1.close()
+    dup_read2.close()
 
     duplicate_ratio = (input_reads - output_reads) * 100 / input_reads
     print(
-        f"input read pairs: {input_reads}\noutput read pairs: {output_reads}\nduplicate ratio: {duplicate_ratio:.2f}")
+        f"input read pairs: {input_reads}\noutput read pairs: {output_reads}\nduplicate ratio: {duplicate_ratio:.2f}%")
 
 
 if __name__ == "__main__":
@@ -50,9 +56,14 @@ if __name__ == "__main__":
                         type=str, required=True, help="output read1.fq.gz")
     parser.add_argument("-o2", dest="output2",
                         type=str, required=True, help="output read2.fq.gz")
+    parser.add_argument("-d1", dest="duplicate1", type=str,
+                        default=os.devnull, help="output read1.dup.fq.gz")
+    parser.add_argument("-d2", dest="duplicate2", type=str,
+                        default=os.devnull, help="output read2.dup.fq.gz")
     args = parser.parse_args()
 
-    print(f"processing {args.input1} {args.input2}")
+    print(f"processing {args.input1} {args.input2} ...")
 
     main(gzip.open(args.input1, "rb"), gzip.open(args.input2, "rb"),
-         gzip.open(args.output1, "wb"), gzip.open(args.output2, "wb"))
+         gzip.open(args.output1, "wb"), gzip.open(args.output2, "wb"),
+         gzip.open(args.duplicate1, "wb"), gzip.open(args.duplicate2, "wb"))
--- remove_duplicates.sh
+++ remove_duplicates.sh
@@ -22,10 +22,23 @@ uncompress2=${prefix}.temp/R2.undecompress.fastq
 mkdir -p $tempdir
 pigz -p $threads -d -c $read1 > $decompressed1
 pigz -p $threads -d -c $read2 > $decompressed2
+
+echo processing $read1 $read2 ...
+echo -e "input read pairs: \c"
+((input_num=`cat $decompressed1 | wc -l` / 4))
+echo $input_num
+
 paste $decompressed1 $decompressed2 | \
     awk '{if(NR%4==0){printf $0"\n"}else{printf $0"\t"}}' | \
-    sort -T $tempdir -t $'\t' -k3,3 -u | \
+    sort -T $tempdir -t $'\t' -k3,4 -u | \
     awk -v FS='\t' '{print $1"\n"$3"\n"$5"\n"$7 > "'$uncompress1'"; print $2"\n"$4"\n"$6"\n"$8 > "'$uncompress2'"}'
+
+echo -e "output read pairs: \c"
+((output_num=`cat $uncompress1 | wc -l` / 4))
+echo $output_num
+
+echo -e "duplicate ratio: \c"
+awk 'BEGIN{printf"%.2f%\n",('$[$input_num-$output_num]'/'$input_num')*100}'
 pigz -p $threads -c $uncompress1 > $out1
 pigz -p $threads -c $uncompress2 > $out2
 
