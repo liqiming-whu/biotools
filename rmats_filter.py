5fe52ac50996983f03cfab501513a80dc00e2766
liqiming-whu <liqiming@whu.edu.cn>
2021-05-24 20:53:18 +0800

    first_commit

--- rmats_filter.py
+++ rmats_filter.py
@@ -5,13 +5,19 @@ import argparse
 import pandas as pd
 
 
-def main(inf, outf, topnumber, p_value=0.05, fdr_value=None, inclevel=None):
+def main(inf, outf, topnumber, p_value=None, readnumber=None, fdr_value=None, inclevel=None):
     df = pd.read_csv(inf, header=0, delimiter="\t")
-    df = df[df['PValue'] < p_value]
+    if p_value:
+        df = df[df['PValue'] < p_value]
     if fdr_value:
         df = df[df['FDR'] < fdr_value]
     if inclevel:
         df = df[abs(df['IncLevelDifference']) > inclevel]
+    
+    if readnumber:
+        df['ReadNumber'] = df['IJC_SAMPLE_1'] + df['SJC_SAMPLE_1'] + df['IJC_SAMPLE_2'] + df['SJC_SAMPLE_2']
+        df = df[df['ReadNumber'] > readnumber]
+        df.drop('ReadNumber', axis=1, inplace=True)
 
     df['sort_key'] = df.IncLevelDifference.abs()
 
@@ -20,7 +26,8 @@ def main(inf, outf, topnumber, p_value=0.05, fdr_value=None, inclevel=None):
 
     if topnumber:
         df = df[:topnumber]
-    df.to_csv(outf, sep="\t", index=False)
+    if outf:
+        df.to_csv(outf, sep="\t", index=False)
 
     print(f"{df.shape[0]} lines.")
 
@@ -32,17 +39,19 @@ if __name__ == '__main__':
     parser.add_argument("-i", "--input", dest="input_file", type=str,
                         required=True, help="Input file gnerated by rmats")
     parser.add_argument("-o", "--output", dest="output_file",
-                        type=str, help="Output file")
+                        type=str, default=None, help="Output file")
     parser.add_argument("-p", "--pvalue", dest="pvalue",
-                        type=float, default=0.05, help="Pvalue cutoff")
+                        type=float, default=None, help="Pvalue cutoff")
     parser.add_argument("-f", "--fdr", dest="fdr",
                         type=float, default=None, help="FDR value cutoff")
     parser.add_argument("-l", "--inclevel", dest="inclevel",
                         type=float, default=0.01, help="IncLevel Difference cutoff")
+    parser.add_argument("-r", "--readnumber", dest="readnumber",
+                        type=int, default=None, help="Reads number cutoff")
     parser.add_argument("-n", "--number", dest="topnumber",
                         type=int, default=None, help="How many records to save")
 
     args = parser.parse_args()
 
-    main(args.input_file, args.output_file, args.topnumber,
+    main(args.input_file, args.output_file, args.topnumber, args.readnumber,
          args.pvalue, args.fdr, args.inclevel)
